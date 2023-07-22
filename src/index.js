8680bc6aa3712380b4e911fef6557cba36195ca1
NGALA MAC <macngala4@gmail.com>
2023-07-22 09:56:05 +0100

    Update source files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // index.js
 
 import './style.css';
-import { tasks, addTask, deleteTask, editTaskDescription, saveToLocalStorage, loadFromLocalStorage } from './todoFunctions';
+import { data, addTask, deleteTask, editTaskDescription, loadFromLocalStorage } from './todoFunctions.js';
+import { updateStatus, clearCompletedTasks } from '../modules/statusUpdates.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadFromLocalStorage();
-  populateTodoList(tasks);
+  populateTodoList(data.tasks);
+
+  // ...
 
   // Event listener for adding a new task
   const todoInput = document.getElementById('todo-input');
@@ -15,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (description !== '') {
         addTask(description);
         saveToLocalStorage();
-        populateTodoList(tasks);
+        populateTodoList(data.tasks);
         event.target.value = '';
       }
     }
@@ -26,75 +29,23 @@ document.addEventListener('DOMContentLoaded', () => {
   todoList.addEventListener('change', (event) => {
     if (event.target.classList.contains('task-completed')) {
       const index = parseInt(event.target.dataset.index);
-      tasks[index - 1].completed = event.target.checked;
-      if (event.target.checked) {
+      const completed = event.target.checked;
+      updateStatus(index, completed);
+      if (completed) {
         addDeleteButton(index);
       } else {
         removeDeleteButton(index);
       }
-      saveToLocalStorage();
-    }
-  });
-
-  // Event listener for deleting a task using the delete button
-  todoList.addEventListener('click', (event) => {
-    if (event.target.classList.contains('delete-btn')) {
-      const index = parseInt(event.target.dataset.index);
-      deleteTask(index);
-      saveToLocalStorage();
-      populateTodoList(tasks);
+      populateTodoList(data.tasks);
     }
   });
 
   // Event listener for clearing all completed tasks
   const clearCompletedBtn = document.getElementById('clear-completed-btn');
   clearCompletedBtn.addEventListener('click', () => {
-    tasks.filter((task) => task.completed).forEach((task) => {
-      const index = tasks.indexOf(task);
-      deleteTask(index + 1);
-    });
-    saveToLocalStorage();
-    populateTodoList(tasks);
+    clearCompletedTasks();
+    populateTodoList(data.tasks);
   });
 });
 
-function populateTodoList(tasks) {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = '';
-
-  tasks.forEach((task, index) => {
-    const li = document.createElement('li');
-    li.className = 'todo-item';
-    li.innerHTML = `
-      <input type="checkbox" class="task-completed" data-index="${task.index}" ${task.completed ? 'checked' : ''}>
-      <span class="task-description" data-index="${task.index}" contenteditable>${task.description}</span>
-      <div class="task-actions">
-        <span class="vertical-dots">&#8942;</span>
-      </div>
-    `;
-    todoList.appendChild(li);
-
-    if (task.completed) {
-      addDeleteButton(task.index);
-    }
-  });
-}
-
-function addDeleteButton(index) {
-  const taskItem = document.querySelector(`[data-index="${index}"]`).closest('.todo-item');
-  if (!taskItem.querySelector('.delete-btn')) {
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete-btn';
-    deleteBtn.dataset.index = index;
-    deleteBtn.textContent = 'Delete';
-    taskItem.querySelector('.task-actions').appendChild(deleteBtn);
-  }
-}
-
-function removeDeleteButton(index) {
-  const taskItem = document.querySelector(`[data-index="${index}"]`).closest('.todo-item');
-  const deleteBtn = taskItem.querySelector('.delete-btn');
-  if (deleteBtn) {
-    deleteBtn.remove();
-  }
-}
+// ...
--- src/todoFunctions.js
+++ src/todoFunctions.js
@@ -1,38 +1,37 @@
 // todoFunctions.js
 
-export const tasks = [];
-
-export function addTask(description) {
-  const newTask = {
-    description,
-    completed: false,
-    index: tasks.length + 1,
+export let data = {
+    tasks: []
   };
-  tasks.push(newTask);
-}
-
-export function deleteTask(index) {
-  tasks.splice(index - 1, 1);
-  updateIndexes();
-}
-
-export function editTaskDescription(index, newDescription) {
-  tasks[index - 1].description = newDescription;
-}
-
-export function updateIndexes() {
-  tasks.forEach((task, index) => {
-    task.index = index + 1;
-  });
-}
-
-export function saveToLocalStorage() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-export function loadFromLocalStorage() {
-  const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-  if (storedTasks) {
-    tasks.push(...storedTasks);
+  
+  // Function to add a new task
+  export function addTask(description) {
+    const newTask = {
+      description,
+      completed: false,
+      index: data.tasks.length + 1
+    };
+    data.tasks.push(newTask);
   }
-}
+  
+  // Function to delete a task
+  export function deleteTask(index) {
+    data.tasks.splice(index - 1, 1);
+  }
+  
+  // Function to edit a task description
+  export function editTaskDescription(index, description) {
+    data.tasks[index - 1].description = description;
+  }
+  
+  // Function to save tasks to local storage
+  export function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(data.tasks));
+  }
+  
+  // Function to load tasks from local storage
+  export function loadFromLocalStorage() {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    data.tasks = storedTasks || [];
+  }
+  
